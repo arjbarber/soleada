bca6f69872e431337327c3a383e08ca76c49563b
Kriti-Negi <66047981+Kriti-Negi@users.noreply.github.com>
2026-03-28 15:21:20 -0400

    schema updated + login signup endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,17 @@
 require('dotenv').config();
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require("mongoose")
 const uri = process.env.MONGO_URL;
 const bodyParser = require("body-parser");
+const express = require("express");
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"));
 
 // schema setup
 const userSchema = new mongoose.Schema({
@@ -47,14 +45,20 @@ const chatSchema = new mongoose.Schema({
 
 const Posts = new mongoose.model("Posts", postSchema);
 const Users = new mongoose.model("Users", userSchema);
+const Chat = new mongoose.model("Chats", chatSchema);
 
-// get Stuff
+app.get("/login/:username/:pass", async (req, res) => {
+    
+    let usernameGiven = req.params.username;
+    let passwordGiven = req.params.pass;
 
-app.get("/login", (req, res) => {
-    let usernameGiven = req.body.username;
-    let passwordGiven = req.body.password;
-    Users.find({username: usernameGiven, password: passwordGiven}, (err,result) => {
-        if(!err){
+    if(!usernameGiven || !passwordGiven || usernameGiven == null || passwordGiven == null){
+        return res.json({success: false, result: null});
+    }
+    console.log("sent");
+    Users.find({username: usernameGiven, password: passwordGiven}).then((result) => {
+        if(result && result.length > 0){
+            console.log(result);
             return res.json({success: true, result: result})
         }else{
             return res.json({success: false, result: null});
@@ -63,66 +67,21 @@ app.get("/login", (req, res) => {
 });
 
 
-app.post("/signup", (req, res) => {
-    let usernameGiven = req.body.username;
-    let passwordGiven = req.body.password;
-    let userType = req.body.userType;
+app.get("/signup/:username/:pass/:type", (req, res) => {
+    let usernameGiven = req.params.username;//req.body.username;
+    let passwordGiven = req.params.pass;//req.body.password;
+    let userType = req.params.type;//body.userType;
 
-    let newUser = new toBeApproved({
+    let newUser = new Users({
         username: usernameGiven,
         password: passwordGiven, 
         type: userType
     });
 
-    new newUser.save().then(() => {
+    newUser.save().then(() => {
         return res.json(newUser);
     });
 })
 
-app.get("/postByAuthorId", (req, res) => {
-    let id = req.body.authorId;
-    Posts.find({authorId: id}, (err, results) => {
-        if(!err){
-            return res.json(results);
-        }else{
-            return res.json([]);
-        }
-    })
-})
 
-app.get("/postByName", (req, res) => {
-    let searchTerm = req.body.search;
-    searchTerm = searchTerm.toLowerCase();
-    let searchDomain = req.body.userType;
-    
-    Posts.find({name : /searchTerm/}, (err, results) => {
-        if(!err){
-            return res.json(results);
-        }else{
-            return res.json([]);
-        }
-    })
-    
-});
-
-// create post
-app.post("/", (req, res) =>{
-
-});
-
-/*default code*/
-async function run() {
-  try {
-    // Connect the client to the server   (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-run().catch(console.dir);
+app.listen(3000);
